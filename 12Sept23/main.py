1b28f8046586a7360b98f06001b4810e2cd2ad3c
Achmad Fawaz Ferdiansyah <47052717+Herpeto@users.noreply.github.com>
2023-09-25 10:57:28 +0700

    Delete 12Sept23 directory

--- 12Sept23/main.py
+++
@@ -1,8 +0,0 @@
-# main.py
-from my_module import * # Menggunakan fungsi dari my_module
-message = greet("Alice")
-print(message) # Menggunakan variabel dari my_module
-
-radius = 5
-area = pi * square(radius)
-print(f"Luas lingkaran dengan radius {radius} adalah {area:.2f}")
--- 12Sept23/my_module.py
+++
@@ -1,7 +0,0 @@
-# my_module.py
-def greet(name):    
-    return f"Hello, {name}!"
-    
-pi = 3.14159265359
-def square(x):    
-    return x ** 2
